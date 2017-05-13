67b122bef4b6e05c8317f9742c9d71b9be611b1d
Keith Griffiths <info@keith-griffiths.com>
2017-05-13 08:13:12 +0100

    Create SysInfo.bat

---
+++ SysInfo.bat
@@ -0,0 +1,8 @@
+//Created by Keith Griffiths 2017
+
+Echo #Description		#Output >> SysInfo.txt
+Echo Date			%date% >> SysInfo.txt
+Echo Time			%time% >> SysInfo.txt
+
+systeminfo >> SysInfo.txt
+getmac >> SysInfo.txt
